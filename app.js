98029948fb2093e7a6d818fccddd2e6fb10db8cf
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2016-11-20 22:42:18 +0000

    Rework

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-var Connection = require('tedious').Connection;  
-    var config = {  
-        userName: 'dev',  
-        password: 'Tsense696969',  
-        server: 'remote.gpereira.tk'
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+
+    var sql = require("mssql");
+    var config = {
+        user: 'dev',
+        password: 'Tsense696969',
+        server: 'remote.gpereira.tk',
+        database: "teste"
         // If you are on Microsoft Azure, you need this:  
         //options: {encrypt: true, database: 'AdventureWorks'}  
-    };  
-    var connection = new Connection(config);  
-    connection.on('connect', function(err) {  
-    // If no error, then good to proceed.  
-        console.log("Connected");  
+    };
+    // connect to your database
+    sql.connect(config, function (err) {
+
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+
+        // query to the database and get the records
+        request.query('INSERT * from dbo.tsense', function (err, recordset) {
+
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+
+        });
     });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
