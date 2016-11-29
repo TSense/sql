b4ae9b235f3db82d84649ebca376eabc745b9a89
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2016-11-29 13:17:11 +0000

    evilscan working

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var sql = require("mssql"); //docs: https://www.npmjs.com/package/mssql
 var request = require("request");
+var evilscan = require("evilscan");
+
+//array of sensors ip
+var sensors = [];
+
+var options = {
+    target: '192.168.1.0/24',
+    port: '7568',
+    status: 'O', // Timeout, Refused, Open, Unreachable
+    banner: true
+};
 
 // configurable variables
 var configs = {
@@ -7,7 +18,7 @@ var configs = {
     limitHigh: 23.50,
     readInterval: 5 // seconds
 }
-   
+
 var db = {
     user: 'dev',
     password: 'arroz',
@@ -16,25 +27,36 @@ var db = {
     database: "tsense"
 };
 
+var scanner = new evilscan(options);
+ 
+scanner.on('result',function(data) {
+    sensors.push(data.ip); //TODO: filter for ESPS and not all ips with port 80 open
+    console.log(data);
+});
+
+scanner.run(); // Run this whenever needed to recheck for new ESP's'
+
 // connect to your database
 sql.connect(db, function (err) {
-
     if (err) console.log(err);
 
-    setInterval(function(){
-        request("http://192.168.1.82:7568/temp", function (error, response, body) {
-            if (!error) {
-                // create Request object
-                var request = new sql.Request();
+    setInterval(function () {
+        sensors.forEach(function (sensor) {
+            request("http://" + sensor + ":" + options.port, function (error, response, body) {
+                if (!error) {
+                    // create Request object
+                    var request = new sql.Request();
 
-                // query to the database and get the records
-                request.query('INSERT INTO data ("device", "timestamp", "temperature", "humidity") VALUES ('+body.split(";")[0]+', CURRENT_TIMESTAMP,'+body.split(";")[1]+','+body.split(";")[1]+');', function (err, recordset) {
+                    // query to the database and get the records
+                    request.query('INSERT INTO data ("device", "timestamp", "temperature", "humidity") VALUES (' + body.split(";")[0] + ', CURRENT_TIMESTAMP,' + body.split(";")[1] + ',' + body.split(";")[1] + ');', function (err, recordset) {
 
-                    if (err) console.log(err)
-                });
-            } else {
-                console.log(error);
-            }
+                        if (err) console.log(err)
+                    });
+                } else {
+                    console.log(error);
+                }
+            });
         });
-    }, configs.readInterval*1000);
+
+    }, configs.readInterval * 1000);
 });
